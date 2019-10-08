c290b9eb4e66cf987b11859373cf4c1a41e387c2
Kacper Wasiak <kacperjacekwasiak@gmail.com>
2019-10-08 11:38:56 +0200

    add move right

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const board = document.querySelector('.board');
 const cellHeight = 20;
 const cellWidth = 20;
-const cellNumberColumns = 30;
-const cellNumberRows = 30;
+const cellNumberColumns = 15;
+const cellNumberRows = 15;
+let lastKeyCode = '';
 
 const cellsArr = [];
 
@@ -11,10 +12,13 @@ const foodPosition = {
   y: 5,
 }
 const snakePosition = {
-  x: [4],
-  y: [3],
+  x: [4, 4],
+  y: [2, 3],
   xy: [],
-
+}
+const snakeV = {
+  x: 1,
+  y: 1,
 }
 
 board.style.width = cellWidth * cellNumberColumns + 'px';
@@ -53,36 +57,64 @@ const food = () => {
 }
 
 const snake = () => {
+
   cellsArr.forEach(cell => {
+    cell.classList.remove('snake');
     const cellDataArr = cell.dataset.key.split('-');
-    if (cellDataArr[0] === snakePosition.x.toString() && cellDataArr[1] === snakePosition.y.toString()) {
-      cell.classList.add('snake');
-      // snakePosition.xy.push(cell.dataset.key)
+    if (snakePosition.x.find(x => {
+
+        return x.toString() === cellDataArr[0]
+      }) && snakePosition.y.find(y => {
+        return y.toString() === cellDataArr[1]
+      })) {
+      cell.classList.add('snake')
     }
+
   })
 }
 
-const move = (e) => {
-  cellsArr.forEach(cell => {
-    cell.classList.remove('snake')
-  })
-  if (e.keyCode === 37) {
-    snakePosition.x--;
-  } else if (e.keyCode === 38) {
-    snakePosition.y--;
-  } else if (e.keyCode === 39) {
-    snakePosition.x++;
-  } else if (e.keyCode === 40) {
-    snakePosition.y++;
-  }
+const moveSnake = () => {
+
+}
+const moveRight = () => {
+  // zmieniam pozycje x
+  let lastItemX = snakePosition.x[snakePosition.x.length - 1];
+  lastItemX++;
+  snakePosition.x.push(lastItemX);
+  snakePosition.x.shift(0, 1)
+  // zmieniam pozycje y
+  let lastItemY = snakePosition.y[snakePosition.y.length - 1];
+  snakePosition.y.push(lastItemY)
+  snakePosition.y.shift(0, 1)
+  // rysuje weza
   snake()
 }
+
+const move = (e) => {
+  if (e.keyCode === 37) {
+    if (lastKeyCode === 37) return;
+    lastKeyCode = e.keyCode;
+  } else if (e.keyCode === 38) {
+    snakePosition.y--;
+    lastKeyCode = e.keyCode;
+  } else if (e.keyCode === 39) {
+    if (lastKeyCode === 39 || lastKeyCode === 37) return;
+    moveRight()
+    lastKeyCode = e.keyCode;
+  } else if (e.keyCode === 40) {
+    snakePosition.y++;
+    lastKeyCode = e.keyCode;
+  }
+}
+
+
 
 const draw = () => {
   drawBoard()
   drawPositionFood()
   food()
   snake()
+  setInterval(moveRight, 100)
 }
 
 draw()
